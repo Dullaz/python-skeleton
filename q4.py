ea8a4fc0c61100fcb673da4c9119a9f78c445b02
Dullaz <dulla2go@gmail.com>
2018-10-17 14:57:27 +0100

    Update Q4.py v1.0

--- q4.py
+++ q4.py
@@ -5,6 +5,24 @@ import numpy as np
 #compute all combinations for two portfolios
 def question04(rows, numberMachines):
   # modify and then return the variable below
-  answer = -1
+  minn = 100000
+  tempmin = 100000
+  innermin = 100000
+  
+  for i in rows:
+    tempmin = 100000
+    for j in range(len(i) - numberMachines):
+      innermin = 0
+      for k in range(numberMachines):
+        if(not isinstance(i[j+k],int)):
+           innermin = 100000
+           break;
+        innermin += i[j+k]
+      
+      if(innermin < tempmin):
+           tempmin = innermin
+      
+    if(tempmin < minn):
+           minn=tempmin
+  answer = minn
   return answer
-
